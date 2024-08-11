b4e3827b74e2f3d0bc306f7ab253eafebfd18c8d
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-08-11 13:26:30 +0800

    feat: api endpoint for listing and transaction

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\API\ListingController;
+use App\Http\Controllers\API\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return response()->json([
         'success' => true,
-        'message' => 'Current user loged in',
+        'message' => 'Current user logged in',
         'data' => $request->user(),
     ]);
 });
+
+Route::resource('listing', ListingController::class)->only(['index', 'show']);
+
+Route::post('transaction/is-available', [TransactionController::class, 'isAvailable'])->middleware(['auth:sanctum']);
+Route::resource('transaction', TransactionController::class)->only(['store', 'index', 'show'])->middleware(['auth:sanctum']);
 
 require __DIR__.'/auth.php';
